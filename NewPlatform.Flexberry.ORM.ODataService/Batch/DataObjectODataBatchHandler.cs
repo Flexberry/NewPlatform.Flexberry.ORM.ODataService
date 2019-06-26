40407211b1a14f321a44dc6e7fe0ff37a1e45a3a
Bratchikov Igor <bratchikov@ics.perm.ru>
2019-06-26 17:12:37 +0500

    Add batch requests support

---
+++ NewPlatform.Flexberry.ORM.ODataService/Batch/DataObjectODataBatchHandler.cs
@@ -0,0 +1,107 @@
+﻿namespace NewPlatform.Flexberry.ORM.ODataService.Batch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.OData.Batch;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+
+    /// <summary>
+    /// Batch handler for DataService.
+    /// </summary>
+    internal class DataObjectODataBatchHandler : DefaultODataBatchHandler
+    {
+        /// <summary>
+        /// Initializes a new instance of the NewPlatform.Flexberry.ORM.ODataService.Batch.DataObjectODataBatchHandler class.
+        /// </summary>
+        /// <param name="httpServer">The System.Web.Http.HttpServer for handling the individual batch requests.</param>
+        public DataObjectODataBatchHandler(HttpServer httpServer)
+            : base(httpServer)
+        {
+        }
+
+        /// <summary>
+        /// Request Properties collection key for DataObjectsToUpdate list.
+        /// </summary>
+        public const string DataObjectsToUpdatePropertyKey = "DataObjectsToUpdate";
+
+        /// <inheritdoc />
+        public async override Task<IList<ODataBatchResponseItem>> ExecuteRequestMessagesAsync(
+                   IEnumerable<ODataBatchRequestItem> requests,
+                   CancellationToken cancellation)
+        {
+            if (requests == null)
+            {
+                throw new ArgumentNullException("requests");
+            }
+
+            IList<ODataBatchResponseItem> responses = new List<ODataBatchResponseItem>();
+            try
+            {
+                foreach (ODataBatchRequestItem request in requests)
+                {
+                    var operation = request as OperationRequestItem;
+                    if (operation != null)
+                    {
+                        responses.Add(await request.SendRequestAsync(Invoker, cancellation));
+                    }
+                    else
+                    {
+                        await ExecuteChangeSet((ChangeSetRequestItem)request, responses, cancellation);
+                    }
+                }
+            }
+            catch
+            {
+                foreach (ODataBatchResponseItem response in responses)
+                {
+                    if (response != null)
+                    {
+                        response.Dispose();
+                    }
+                }
+
+                throw;
+            }
+
+            return responses;
+        }
+
+        /// <summary>
+        /// Execute changeset processing.
+        /// </summary>
+        /// <param name="changeSet">Changeset for processing.</param>
+        /// <param name="responses">Responses for each request.</param>
+        /// <param name="cancellation">Cancelation token.</param>
+        /// <returns>Task for changeset processing.</returns>
+        private async Task ExecuteChangeSet(ChangeSetRequestItem changeSet, IList<ODataBatchResponseItem> responses, CancellationToken cancellation)
+        {
+            ChangeSetResponseItem changeSetResponse;
+
+            List<DataObject> dataObjectsToUpdate = new List<DataObject>();
+
+            foreach (HttpRequestMessage request in changeSet.Requests)
+            {
+                if (!request.Properties.ContainsKey(DataObjectsToUpdatePropertyKey))
+                {
+                    request.Properties.Add(DataObjectsToUpdatePropertyKey, dataObjectsToUpdate);
+                }
+            }
+
+            changeSetResponse = (ChangeSetResponseItem)await changeSet.SendRequestAsync(Invoker, cancellation);
+            responses.Add(changeSetResponse);
+
+            if (changeSetResponse.Responses.All(r => r.IsSuccessStatusCode))
+            {
+                IDataService ds = DataServiceProvider.DataService;
+                DataObject[] dataObjects = dataObjectsToUpdate.ToArray();
+                ds.UpdateObjects(ref dataObjects);
+            }
+        }
+    }
+}
