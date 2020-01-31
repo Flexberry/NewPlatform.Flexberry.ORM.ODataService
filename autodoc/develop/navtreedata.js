fc4c97d73722ee5ba3dc67e9a3dcf1eface6e5f1
Flexberry-man <mail@flexberry.net>
2020-01-31 10:58:54 +0000

    Update gh-pages for develop branch

--- autodoc/develop/navtreedata.js
+++ autodoc/develop/navtreedata.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "Flexberry ORM ODataService 5.1.0-beta02", "index.html", [
+  [ "Flexberry ORM ODataService 5.1.0-beta03", "index.html", [
     [ "Пространства имен", null, [
       [ "Пространства имен", "namespaces.html", "namespaces" ],
       [ "Члены пространств имен", "namespacemembers.html", [
