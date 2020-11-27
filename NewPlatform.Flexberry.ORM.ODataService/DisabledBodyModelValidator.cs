fb8d6314e63483038c127236175cd0db84abdd80
Bratchikov Igor <bratchikov@ics.perm.ru>
2020-11-27 23:51:31 +0500

    Add DisabledBodyModelValidator

---
+++ NewPlatform.Flexberry.ORM.ODataService/DisabledBodyModelValidator.cs
@@ -0,0 +1,19 @@
+﻿#if NETFRAMEWORK
+namespace NewPlatform.Flexberry.ORM.ODataService
+{
+    using System;
+    using System.Web.Http.Validation;
+
+    /// <summary>
+    /// Валидатор с отключенной проверкой типов.
+    /// </summary>
+    public class DisabledBodyModelValidator : DefaultBodyModelValidator
+    {
+        /// <inheritdoc/>
+        public override bool ShouldValidateType(Type type)
+        {
+            return false;
+        }
+    }
+}
+#endif
