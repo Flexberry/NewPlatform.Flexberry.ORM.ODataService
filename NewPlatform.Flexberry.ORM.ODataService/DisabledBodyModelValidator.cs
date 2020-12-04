e2a72f04f30a3d5bd5e3581567d0c5aecf6bc343
Bratchikov Igor <bratchikov@ics.perm.ru>
2020-12-04 23:30:19 +0500

    Add DisabledBodyModelValidator for Core (#206)
    * Add DisabledBodyModelValidator for Core
    
    * Update nuspec

--- NewPlatform.Flexberry.ORM.ODataService/DisabledBodyModelValidator.cs
+++ NewPlatform.Flexberry.ORM.ODataService/DisabledBodyModelValidator.cs
@@ -1,7 +1,8 @@
-﻿#if NETFRAMEWORK
-namespace NewPlatform.Flexberry.ORM.ODataService
+﻿namespace NewPlatform.Flexberry.ORM.ODataService
 {
     using System;
+
+#if NETFRAMEWORK
     using System.Web.Http.Validation;
 
     /// <summary>
@@ -15,5 +16,20 @@ namespace NewPlatform.Flexberry.ORM.ODataService
             return false;
         }
     }
-}
 #endif
+#if NETSTANDARD
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+    /// <summary>
+    /// Валидатор с отключенной проверкой типов.
+    /// </summary>
+    public class DisabledBodyModelValidator : IObjectModelValidator
+    {
+        /// <inheritdoc/>
+        public void Validate(ActionContext actionContext, ValidationStateDictionary validationState, string prefix, object model)
+        {
+        }
+    }
+#endif
+}
