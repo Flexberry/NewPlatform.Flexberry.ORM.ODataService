adeef09e91323172eb63528657721b0b502f1afd
Нерослов Егор Александрович <egor.neroslov@mail.ru>
2022-05-25 13:02:40 +0500

    Checking with identical PublishName

--- Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/BaseIntegratedTest.cs
+++ Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/BaseIntegratedTest.cs
@@ -168,8 +168,7 @@ namespace NewPlatform.Flexberry.ORM.ODataService.Tests
 
             _tempDbNamePrefix = tempDbNamePrefix;
             _databaseName = _tempDbNamePrefix + "_" + DateTime.Now.ToString("yyyyMMddHHmmssff") + "_" + Guid.NewGuid().ToString("N");
-            bool watchdogEmptyTest = true;
-            _dataServices.Add(CreatePostgresDataService($"{connectionStringPostgres};Database={_databaseName}"));
+            bool watchdogEmptyTest = false;
 
             if (!string.IsNullOrWhiteSpace(PostgresScript) && connectionStringPostgres != PoolingFalseConst)
             {
