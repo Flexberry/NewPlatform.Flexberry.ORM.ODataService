25d3476b949d5a8ee31f654bc83942bad9ae5071
Bratchikov Igor <igor@bratchikovy.ru>
2022-09-23 22:51:43 +0500

    Add test TestFilterByDetailMasterMaster

--- Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/CRUD/Read/FilterByMasterMasterDetailFieldTest.cs
+++ Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/CRUD/Read/FilterByMasterMasterDetailFieldTest.cs
@@ -151,5 +151,47 @@ namespace NewPlatform.Flexberry.ORM.ODataService.Tests.CRUD.Read
                 }
             });
         }
+
+        /// <summary>
+        /// Tests filtering details by master master.
+        /// </summary>
+        [Fact]
+        public void TestFilterByDetailMasterMaster()
+        {
+            ActODataService(args =>
+            {
+                // Arrange.
+                Порода breed = new Порода() { Название = "Бурый" };
+                Страна country = new Страна() { Название = "Гана" };
+                string countryPkString = ((KeyGuid)country.__PrimaryKey).Guid.ToString("D");
+                Лес forest = new Лес() { Название = "Тёмный", Страна = country };
+                string forestPkString = ((KeyGuid)forest.__PrimaryKey).Guid.ToString("D");
+                Берлога den = new Берлога() { Наименование = "Под ёлкой", ПодходитДляПороды = breed, ЛесРасположения = forest };
+
+                Медведь bear = new Медведь() { ПорядковыйНомер = 1 };
+                bear.Берлога.Add(den);
+
+                DataObject[] newDataObjects = new DataObject[] { breed, forest, den, bear };
+
+                args.DataService.UpdateObjects(ref newDataObjects);
+                ExternalLangDef.LanguageDef.DataService = args.DataService;
+
+                string requestUrl = string.Format(
+                "http://localhost/odata/{0}?$filter={1}&$select={2}",
+                args.Token.Model.GetEdmEntitySet(typeof(Медведь)).Name,
+                "Берлога/any(f:f/ЛесРасположения/Страна/__PrimaryKey%20eq%20" + countryPkString + ")",
+                "__PrimaryKey,ПорядковыйНомер");
+
+                // Act.
+                using (var response = args.HttpClient.GetAsync(requestUrl).Result)
+                {
+                    // Assert.
+                    string receivedStr = response.Content.ReadAsStringAsync().Result.Beautify();
+                    Dictionary<string, object> receivedDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(receivedStr);
+                    Assert.Equal(1, ((JArray)receivedDict["value"]).Count);
+                    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                }
+            });
+        }
     }
 }
