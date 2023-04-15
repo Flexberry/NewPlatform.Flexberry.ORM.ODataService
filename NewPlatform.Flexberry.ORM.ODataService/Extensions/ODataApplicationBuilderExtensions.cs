0c0c70dc85aca02142339c87fc9f1b663bd32d24
Anisimova2020 <70688166+Anisimova2020@users.noreply.github.com>
2023-04-15 21:39:49 +0500

    Added filter on response

--- NewPlatform.Flexberry.ORM.ODataService/Extensions/ODataApplicationBuilderExtensions.cs
+++ NewPlatform.Flexberry.ORM.ODataService/Extensions/ODataApplicationBuilderExtensions.cs
@@ -2,10 +2,14 @@
 namespace NewPlatform.Flexberry.ORM.ODataService.Extensions
 {
     using System;
+    using System.IO;
+    using System.Net.Mime;
+    using System.Threading.Tasks;
     using Microsoft.AspNet.OData;
     using Microsoft.AspNet.OData.Common;
     using Microsoft.AspNet.OData.Extensions;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Routing;
     using NewPlatform.Flexberry.ORM.ODataService.Middleware;
 
@@ -30,6 +34,11 @@ namespace NewPlatform.Flexberry.ORM.ODataService.Extensions
 
             VerifyODataServiceIsRegistered(app);
 
+            app.Use(async (context, next) =>
+            {
+                await RewriteResponse(context, next);
+            });
+
             return app
                 .UseODataBatching()
                 .UseMiddleware<RequestHeadersHookMiddleware>()
@@ -46,6 +55,57 @@ namespace NewPlatform.Flexberry.ORM.ODataService.Extensions
             if (app.ApplicationServices.GetService(typeof(IPerRouteContainer)) == null)
             {
                 throw Error.InvalidOperation(SRResources.MissingODataServices, nameof(IPerRouteContainer));
+            }
+        }
+
+        /// <summary>
+        /// Removing of extra symbols from response.
+        /// </summary>
+        /// <param name="context">Context of request.</param>
+        /// <param name="next">Next middleware.</param>
+        /// <returns>Formed task.</returns>
+        public static async Task RewriteResponse(HttpContext context, Func<Task> next)
+        {
+            using (var responseBodyStream = new MemoryStream())
+            {
+                var bodyStream = context.Response.Body;
+
+                try
+                {
+                    context.Response.Body = responseBodyStream;
+
+                    await next();
+
+                    responseBodyStream.Seek(0, SeekOrigin.Begin);
+                    var responseBody = new StreamReader(responseBodyStream).ReadToEnd();
+
+                    //Modify the response in some way.
+                    if (context.Response.ContentType.Contains("application/json")
+                        || context.Response.ContentType.Contains("application/xml")
+                        || context.Response.ContentType.Contains("multipart/mixed"))
+                    {
+                        responseBody = responseBody
+                            .Replace("(____.", "(")
+                            .Replace("\"____.", "\"")
+                            .Replace("____.", ".")
+                            .Replace(" Namespace=\"____\"", " Namespace=\"\"");
+                    }
+
+                    using (var newStream = new MemoryStream())
+                    {
+                        var sw = new StreamWriter(newStream);
+                        sw.Write(responseBody);
+                        sw.Flush();
+
+                        newStream.Seek(0, SeekOrigin.Begin);
+
+                        await newStream.CopyToAsync(bodyStream);
+                    }
+                }
+                finally
+                {
+                    context.Response.Body = bodyStream;
+                }
             }
         }
     }
