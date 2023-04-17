24ca683b3b77ec0f68d55574b43cc444fc111013
Anisimova2020 <70688166+Anisimova2020@users.noreply.github.com>
2023-04-17 13:58:44 +0500

    Removed commented code

--- NewPlatform.Flexberry.ORM.ODataService/Model/DefaultDataObjectEdmModelBuilder.cs
+++ NewPlatform.Flexberry.ORM.ODataService/Model/DefaultDataObjectEdmModelBuilder.cs
@@ -324,7 +324,7 @@
 
             string typeName = BuildEntityTypeName(dataObjectType);
             string nameSpace = BuildEntityTypeNamespace(dataObjectType);
-            return string.Concat(_useNamespaceInEntitySetName ? $"{nameSpace}.{typeName}".Replace(".", string.Empty) : typeName, "s"/* "Aliases"*/).Replace("_", string.Empty);
+            return string.Concat(_useNamespaceInEntitySetName ? $"{nameSpace}.{typeName}".Replace(".", string.Empty) : typeName, "s").Replace("_", string.Empty);
         }
 
         /// <summary>
