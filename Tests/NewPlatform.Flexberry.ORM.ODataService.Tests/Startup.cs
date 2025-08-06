1ee15a0fdfcc816601a5b2242e47cf9a0278a3eb
BobrikovMikhail <mihapir4t@yandex.ru>
2025-08-06 18:25:15 +0500

    обновлен startup, пока что используем unity.

--- Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/Startup.cs
+++ Tests/NewPlatform.Flexberry.ORM.ODataService.Tests/Startup.cs
@@ -7,16 +7,15 @@ namespace ODataServiceSample.AspNetCore
     using ICSSoft.STORMNET.Business;
     using ICSSoft.STORMNET.Business.Audit;
     using ICSSoft.STORMNET.Business.Interfaces;
-    using ICSSoft.STORMNET.Windows.Forms;
     using ICSSoft.STORMNET.Security;
     using IIS.Caseberry.Logging.Objects;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Hosting.Server.Features;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Options;
+    using Moq;
+    using NewPlatform.Flexberry;
     using NewPlatform.Flexberry.ORM.ODataService.Extensions;
     using NewPlatform.Flexberry.ORM.ODataService.Files;
     using NewPlatform.Flexberry.ORM.ODataService.Model;
@@ -25,11 +24,9 @@ namespace ODataServiceSample.AspNetCore
     using NewPlatform.Flexberry.ORM.ODataServiceCore.Common.Exceptions;
     using NewPlatform.Flexberry.ORM.ODataServiceCore.Extensions;
     using NewPlatform.Flexberry.Services;
-    using System;
-    using System.Linq;
     using Unity;
+    using Unity.Injection;
     using LockService = NewPlatform.Flexberry.Services.LockService;
-    using AdvLimit.ExternalLangDef;
 
     public class Startup
     {
@@ -39,31 +36,73 @@ namespace ODataServiceSample.AspNetCore
         }
 
         private IApplicationBuilder ApplicationBuilder { get; set; }
+
         private IServerAddressesFeature ServerAddressesFeature { get; set; }
+
         public IConfiguration Configuration { get; }
 
         public string CustomizationString => "";
 
+        /// <summary>
+        /// Method for Unity container configuring.
+        /// </summary>
+        /// <param name="unityContainer">Unity container.</param>
+        public virtual void ConfigureContainer(IUnityContainer unityContainer)
+        {
+            // Configure Flexberry services via Unity.
+            unityContainer.RegisterFactory<IBusinessServerProvider>(new Func<IUnityContainer, object>(o => new BusinessServerProvider(new UnityServiceProvider(o))), FactoryLifetime.Singleton);
+            var securityManager = new EmptySecurityManager();
+            Mock<IAuditService> mockAuditService = new Mock<IAuditService>();
+            IBusinessServerProvider businessServerProvider = unityContainer.Resolve<IBusinessServerProvider>();
+            IDataService dataService = new PostgresDataService(securityManager, mockAuditService.Object, businessServerProvider) { CustomizationString = CustomizationString };
+
+            unityContainer.RegisterType<DataObjectEdmModelDependencies>(
+                new InjectionConstructor(
+                    unityContainer.IsRegistered<IExportService>() ? unityContainer.Resolve<IExportService>() : null,
+                    unityContainer.IsRegistered<IExportService>("Export") ? unityContainer.Resolve<IExportService>("Export") : null,
+                    unityContainer.IsRegistered<IExportStringedObjectViewService>() ? unityContainer.Resolve<IExportStringedObjectViewService>() : null,
+                    unityContainer.IsRegistered<IExportStringedObjectViewService>("ExportStringedObjectView") ? unityContainer.Resolve<IExportStringedObjectViewService>("ExportStringedObjectView") : null,
+                    unityContainer.IsRegistered<IODataExportService>() ? unityContainer.Resolve<IODataExportService>() : null,
+                    unityContainer.IsRegistered<IODataExportService>("Export") ? unityContainer.Resolve<IODataExportService>("Export") : null));
+            unityContainer.RegisterInstance(dataService);
+            unityContainer.RegisterInstance<ILockService>(new LockService(dataService));
+            unityContainer.RegisterInstance<ISecurityManager>(new EmptySecurityManager());
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public virtual void ConfigureServices(IServiceCollection services)
         {
-            // Регистрируем MVC с отключённым endpoint routing, добавляем фильтр
+            /*
+            // Configure Flexberry services (LockService and IDataService) via native DI.
+            {
+                services.AddSingleton<IDataService>(provider =>
+                {
+                    IDataService dataService = new PostgresDataService() { CustomizationString = CustomizationString };
+                    ExternalLangDef.LanguageDef.DataService = dataService;
+
+                    return dataService;
+                });
+
+                services.AddSingleton<ILockService, LockService>();
+            }
+            */
+
             services.AddMvcCore(options =>
             {
                 options.Filters.Add<CustomExceptionFilter>();
                 options.EnableEndpointRouting = false;
             })
-            .AddFormatterMappings();
+                .AddFormatterMappings();
 
-            // Регистрируем OData сервисы
             services.AddODataService();
 
-            // Регистрация IDataObjectFileAccessor с использованием ServerAddressesFeature
             services.AddSingleton<IDataObjectFileAccessor>(provider =>
             {
                 Uri baseUri = new Uri("http://localhost");
 
                 if (ServerAddressesFeature != null && ServerAddressesFeature.Addresses != null)
                 {
+                    // This works with pure self-hosted service only.
                     baseUri = new Uri(ServerAddressesFeature.Addresses.Single());
                 }
 
@@ -71,35 +110,17 @@ namespace ODataServiceSample.AspNetCore
 
                 return new DefaultDataObjectFileAccessor(baseUri, "api/File", "Uploads");
             });
-
-            // --- ВАЖНО: Build IServiceProvider для получения настроенных сервисов ---
-            var serviceProvider = services.BuildServiceProvider();
-
-            // Получаем сконфигурированный экземпляр IOptions<MvcOptions> из стандартного DI
-            var mvcOptions = serviceProvider.GetRequiredService<IOptions<MvcOptions>>();
-
-            // Получаем Unity контейнер
-            IUnityContainer unityContainer = UnityFactory.GetContainer();
-
-            // Регистрируем в Unity его же экземпляр IOptions<MvcOptions> для корректной работы MVC через Unity
-            unityContainer.RegisterInstance<IOptions<MvcOptions>>(mvcOptions);
-
-            // Регистрируем Flexberry-сервисы через Unity
-            var ds = unityContainer.Resolve<IDataService>() as PostgresDataService;
-            ds.CustomizationString = CustomizationString;
-            DataServiceProvider.DataService = ds;
-            unityContainer.RegisterType<IDataService, PostgresDataService>();
-            ExternalLangDef.LanguageDef = new ExternalLangDef(ds);
-
-            unityContainer.RegisterInstance<ILockService>(new LockService(ds));
-            unityContainer.RegisterInstance<ISecurityManager>(new EmptySecurityManager());
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Save reference to IApplicationBuilder instance.
             ApplicationBuilder = app;
+
+            // Save reference to IServerAddressesFeature instance.
             ServerAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
-// Используем старый стиль маршрутизации MVC (нужен Disable Endpoint Routing выше)
+
             app.UseMvc(builder =>
             {
                 builder.MapRoute("Lock", "api/lock/{action}/{dataObjectId}", new { controller = "Lock" });
@@ -115,8 +136,8 @@ namespace ODataServiceSample.AspNetCore
                     typeof(UserSetting).Assembly,
                     typeof(Lock).Assembly,
                 };
+                var modelBuilder = new DefaultDataObjectEdmModelBuilder(assemblies, app.ApplicationServices, false);
 
-                var modelBuilder = new DefaultDataObjectEdmModelBuilder(assemblies, app.ApplicationServices);
                 var token = builder.MapDataObjectRoute(modelBuilder);
             });
         }
